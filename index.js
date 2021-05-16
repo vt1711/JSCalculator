26e2c4c41bc7de466989c05335fb8bb73fb629a2
V7777 <vedantt7777@gmail.com>
2021-05-17 01:23:05 +0530

    completed calculator functionality for integers for  all operators with error alerts.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var n = [], s, str, last;
+
+//for manupulating calc display
+var txt = document.querySelector("#text");
+
+//cancel button functionality
 document.getElementById("cancelbtn").addEventListener("click", del);
-function del() {
-  document.getElementById("text").innerHTML = "";
-}
+
+// adding event listeners to calc buttons
 var bts = document.getElementsByTagName("button");
 bts[0].addEventListener("click", function () {
   btnclk(7);
@@ -52,8 +57,10 @@ bts[15].addEventListener("click", function () {
   btnclk("+");
 });
 
+//function for calc button clicks
 function btnclk(i) {
   switch (i) {
+
     case 7:
     case 8:
     case 9:
@@ -63,32 +70,86 @@ function btnclk(i) {
     case 1:
     case 2:
     case 3:
-    case 0:
+      //checkb(i); not required as its function performed during = enter
       textdisp(i);
       break;
+
+    case 0:
+      if (s = '/') {
+        alert("Invalid Operation !\nCannot divide by 0 ");
+      }
+      else {
+        textdisp(i);
+      }
+      break;
+
     case "+":
-    case "-":
     case "/":
+    case "-":
     case "*":
-    case "=":
     case ".":
-      if (check(i)) textdisp(i);
+      if (checko(i)) {
+        insarr1(i);
+        textdisp(i);
+      }
+      break;
+
+    case "=":
+      if (checke(i)) {
+        insarr2();  //inserts 2nd operand to array
+        dispresult(); // displays final result
+      }
       break;
   }
 }
-function textdisp(i) {
-  var num1 = i.toString();
-  var a = document.querySelector("#text").innerHTML;
-  var num2 = a.toString();
-  var fnum = num2.concat(num1);
-  var b = (document.querySelector("#text").innerHTML = fnum);
-  return;
-}
-function check(i) {
-  var a = document.querySelector("#text").innerHTML;
-  var str = a.toString();
-  var last = str.charAt(str.length - 1);
 
+//function to reset calc display
+function del() {
+  txt.innerHTML = "";
+  txt.style.color = 'blanchedalmond';
+  n[0]=""; n[1]="" ; s=""; //required.
+}
+
+//function to display entered content on dispscreen 
+//which takes input , converts current innerhtml and input to string
+//appends input to current innerhtml n displays it
+function textdisp(i) {
+
+  if (txt.style.color == 'rgb(8, 236, 46)') //clears display if it has last result displayd .
+  { del(); }
+  var num1 = i.toString();
+  var num2 = txt.innerHTML.toString();
+  var fnum = num2.concat(num1);
+  txt.innerHTML = fnum;
+  return;
+
+}
+
+//function to check conditions before entering operator to innerhtml n array
+//Returns false if innerhtml  already  contains operator or operator entered at last or
+//operator is entered at 1st ,else return true
+function checko(i) {
+
+  str = txt.innerHTML.toString();
+  var regx1 = /[-|/|+|*]/;
+
+  if (str.charAt(0) == "") {
+    alert("Invalid Expresssion !  \nCannot enter " + i + " at start ");
+    return false;
+  }
+  if (regx1.test(str)) {
+    alert("Invalid Expresssion !  \nCannot have more than 1 operator in a single expression");
+    return false;
+  }
+  else return true;
+}
+
+//function to check conditions before entering equals operator
+//Returns false if last entered value is an operator else return true.
+function checke(i) {
+
+  str = txt.innerHTML.toString();
+  last = str.charAt(str.length - 1);
   if (
     last == "+" ||
     last == "-" ||
@@ -97,11 +158,81 @@ function check(i) {
     last == "=" ||
     last == "."
   ) {
-    alert("Invalid ! Cannot enter " + i + " after " + last);
+    alert("Invalid Expresssion !  \nCannot enter " + s + " at end");
     return false;
   }
-  if (str.charAt(0) == "") {
-    alert("Invalid ! Cannot enter " + i + " at start ");
-    return false;
-  } else return true;
+
+  else return true;
+
+}
+
+//function to insert 1st operand to array and operator to s
+function insarr1(i) {
+
+  n[0] = parseInt(txt.innerHTML);
+  s = i;
+  return;
+}
+
+////////////////////////////////////////////////////////////////////
+// function checkb(i) {
+//   var txt = document.querySelector("#text").innerHTML;
+//   str = txt.toString();
+//   last = str.charAt(str.length - 1);
+//   if (
+//     last == "+" ||
+//     last == "-" ||
+//     last == "/" ||
+//     last == "*" ||
+//     last == "=" ||
+//     last == "."
+//   ) {
+//     n[1] = i;
+//     return;
+//   } else return;
+// }
+//////////////////////////////////////////////////////////////////////////
+
+//function to insert 2nd operand to array on preesing =
+//This find index at which operator occurs and 
+//enters whole string between that operator and end of string to array 
+//and then stores this string as an int in array n. 
+function insarr2() {
+  var regx2 = /[-|/|+|*]/;
+  var str2 = [];
+  str = txt.innerHTML.toString();
+  str3 = str2.toString();
+  for (var i = 0; i < str.length; i++) {
+    if (regx2.test(str[i])) {
+      while (i < str.length - 1) {
+        str3 = str3.concat(str[++i]);
+      }
+      n[1] = parseInt(str3);
+      return;
+    }
+  }
+}
+
+//function to output final result depending on operator
+function dispresult() {
+  var result;
+  txt.style.color = 'rgb(8, 236, 46)';
+  switch (s) {
+    case '+':
+      result = n[0] + n[1];
+      txt.innerHTML = n[0] + "+" + n[1] + "=" + result;
+      break;
+    case '-':
+      result = n[0] - n[1];
+      txt.innerHTML = n[0] + "-" + n[1] + "=" + result;
+      break;
+    case '/':
+      result = n[0] / n[1];
+      txt.innerHTML = n[0] + "/" + n[1] + "=" + result;
+      break;
+    case '*':
+      result = n[0] * n[1];
+      txt.innerHTML = n[0] + "*" + n[1] + "=" + result;
+      break;
+  }
 }
